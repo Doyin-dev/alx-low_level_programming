d8f779df2ed3d471428f0e69287d7392eec40a61
Doyin-dev <140111657+Doyin-dev@users.noreply.github.com>
2023-09-08 00:24:20 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
-#include < stdio.h
-/**
- * main - A program that prints a line with puts function
- * Return:0(Done)
- */
-int main(void)
-{
- char message[] = "\"Programming is like building a multilingual puzzle";	
-	return (0);
-}
+
+/*  
+  *create the main function 
+  * */ 
+ int main(void) 
+ { 
+     char message[] = "\"Programming is like building a multilingual puzzle"; 
+     puts(message); 
+     return (0); 
+ }
