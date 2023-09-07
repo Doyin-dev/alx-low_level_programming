d06aea22186af9b52d17410ff978e1c5a50506cc
Doyin-dev <140111657+Doyin-dev@users.noreply.github.com>
2023-09-08 00:18:09 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,6 +5,6 @@
  */
 int main(void)
 {
-    char message[] = "\"Programming is like building a multilingual puzzle";	
+ char message[] = "\"Programming is like building a multilingual puzzle";	
 	return (0);
 }
