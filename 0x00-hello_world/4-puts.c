fd7464edf8a6cb03d049f923e3bf7297f5396db0
Doyin-dev <140111657+Doyin-dev@users.noreply.github.com>
2023-09-08 01:11:51 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point for the program
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
     char message[] = "\"Programming is like building a multilingual puzzle";
-    
-    /* Print the message without using puts */
     fprintf(stdout, "%s\n\n(52 chars long)\n", message);
-    
-    /* Print the message using puts */
-    puts("Programming is like building a multilingual puzzle");
-
-    return 0;
+    return (0);
 }
 
