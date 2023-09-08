fd76b56a0c7bc9f11caa31be7ea6704aa6a6146e
Doyin-dev <140111657+Doyin-dev@users.noreply.github.com>
2023-09-08 19:59:50 +0100

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,11 +1,14 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
 /**
  * main - Entry point
+ *
  * Description: This program assigns a random number to variable n
- * and prints whether it's positive, zero, or negative.
+ *              and prints whether it's positive, zero, or negative.
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
@@ -15,14 +18,7 @@ int main(void)
     srand(time(0)); /* Seed for random number generation */
     n = rand() - RAND_MAX / 2;
 
-    printf("The number %d is ", n);
-
-    if (n > 0)
-        printf("positive\n");
-    else if (n == 0)
-        printf("zero\n");
-    else
-        printf("negative\n");
+    printf("%d is %s\n", n, n ? (n > 0 ? "positive" : "negative") : "zero");
 
     return (0);
 }
