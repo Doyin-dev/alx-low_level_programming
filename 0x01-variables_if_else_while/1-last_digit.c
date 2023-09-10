d6db00482906a58583a33be506f623d200a5e704
Doyin-dev <140111657+Doyin-dev@users.noreply.github.com>
2023-09-10 18:43:14 +0100

    Update 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,33 +1,30 @@
-#include <stdio.h> 
- #include <stdlib.h> 
- #include <time.h> 
-  
- /** 
-  * main - Entry point 
-  * 
-  * Return: Always 0 (Success) 
-  */ 
- int main(void) 
- { 
-     int n; 
-     int lastDigit; 
-  
-     srand(time(0)); 
-     n = rand() - RAND_MAX / 2; 
-  
-     printf("Last digit of %d is ", n); 
-  
-     if (n < 0) 
-         n = -n; // Make n positive if it's initially negative 
-  
-     lastDigit = n % 10; 
-  
-     if (lastDigit > 5) 
-         printf("%d and is greater than 5\n", lastDigit); 
-     else if (lastDigit == 0) 
-         printf("%d and is 0\n", lastDigit); 
-     else 
-         printf("%d and is less than 6 and not 0\n", lastDigit); 
-  
-     return (0); 
- }
+
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: This program prints the last digit of a number n.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    int n, lastDigit;
+
+    n = 98;
+    lastDigit = n % 10;
+
+    if (lastDigit > 5)
+    {
+        printf("Last digit of %d is %d and is greater than 5\n", n, lastDigit);
+    }
+    else if (lastDigit == 0)
+    {
+        printf("Last digit of %d is %d and is 0\n", n, lastDigit);
+    }
+    else
+    {
+        printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastDigit);
+    }
+
+    return (0);
+}
