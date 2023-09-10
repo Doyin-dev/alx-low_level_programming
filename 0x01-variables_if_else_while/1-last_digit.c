bd4bb836221890346b23425c0dcbd7112eb0b5e4
Doyin-dev <140111657+Doyin-dev@users.noreply.github.com>
2023-09-10 18:48:26 +0100

    Update 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 
 /**
@@ -10,20 +9,22 @@ int main(void)
 {
     int n, lastDigit;
 
-    n = 98;
+    n = -98; // Change the number to -98
     lastDigit = n % 10;
+
+    printf("Last digit of %d is ", n);
 
     if (lastDigit > 5)
     {
-        printf("Last digit of %d is %d and is greater than 5\n", n, lastDigit);
+        printf("%d and is greater than 5\n", lastDigit);
     }
     else if (lastDigit == 0)
     {
-        printf("Last digit of %d is %d and is 0\n", n, lastDigit);
+        printf("%d and is 0\n", lastDigit);
     }
     else
     {
-        printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastDigit);
+        printf("%d and is less than 6 and not 0\n", lastDigit);
     }
 
     return (0);
