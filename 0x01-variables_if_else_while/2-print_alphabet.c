9bb0ae08decfcb99b5c7d979a4be548749d77a05
Doyin-dev <140111657+Doyin-dev@users.noreply.github.com>
2023-09-12 07:15:33 +0100

    Update 2-print_alphabet.c Doyin-dev

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,15 +7,15 @@
  */
 int main(void)
 {
-    char letter = 'a'; /* Initialize a variable to store the letter */
-    
-    while (letter <= 'z') /* Loop through the lowercase alphabet */
-    {
-        putchar(letter); /* Print the current letter using putchar */
-        letter++; /* Move to the next letter */
-    }
-    
-    putchar('\n'); /* Print a newline character to start a new line */
-    
-    return (0); /* Return 0 to indicate success */
+	char letter = 'a'; /* Initialize a variable to store the letter */
+
+	while (letter <= 'z') /* Loop through the lowercase alphabet */
+	{
+		putchar(letter); /* Print the current letter using putchar */
+		letter++; /* Move to the next letter */
+	}
+
+	putchar('\n'); /* Print a newline character to start a new line */
+
+	return (0); /* Return 0 to indicate success */
 }
