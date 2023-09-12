c52a00122f6663a8d306e2ed3e9ef1cae55c349e
Doyin-dev git config lobal user.name Doyin-dev <adedoyinvieve2021@gmail.com>
2023-09-11 23:54:18 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    char letter = 'a'; /* Initialize a variable to store the letter */
+
+    while (letter <= 'z') /* Loop through the lowercase alphabet */
+    {
+        if (letter != 'e' && letter != 'q') /* Check if the letter is not 'e' or 'q' */
+        {
+            putchar(letter); /* Print the current letter using putchar */
+        }
+        letter++; /* Move to the next letter */
+    }
+
+    putchar('\n'); /* Print a newline character to start a new line */
+
+    return (0); /* Return 0 to indicate success */
+}
