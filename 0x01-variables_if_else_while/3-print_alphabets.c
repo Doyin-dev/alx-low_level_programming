994c2930385c3a17ba7dcf49b7568e33563adf81
Doyin-dev git config lobal user.name Doyin-dev <adedoyinvieve2021@gmail.com>
2023-09-11 23:31:14 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    char lowercase = 'a'; /* Initialize a variable for lowercase letters */
+    char uppercase = 'A'; /* Initialize a variable for uppercase letters */
+
+    while (lowercase <= 'z') /* Loop through the lowercase alphabet */
+    {
+        putchar(lowercase); /* Print the current lowercase letter */
+        lowercase++; /* Move to the next lowercase letter */
+    }
+
+    while (uppercase <= 'Z') /* Loop through the uppercase alphabet */
+    {
+        putchar(uppercase); /* Print the current uppercase letter */
+        uppercase++; /* Move to the next uppercase letter */
+    }
+
+    putchar('\n'); /* Print a newline character to start a new line */
+
+    return (0); /* Return 0 to indicate success */
+}
