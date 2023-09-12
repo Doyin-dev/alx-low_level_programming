0eebde59ab50678bbb1f143a7b6ff0d08cac3f6e
Doyin-dev <140111657+Doyin-dev@users.noreply.github.com>
2023-09-12 08:04:33 +0100

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,18 +7,18 @@
  */
 int main(void)
 {
-	char letter = 'a'; /* Initialize a variable to store the letter */
+    char letter = 'a'; /* Initialize a variable to store the letter */
 
-	while (letter <= 'z') /* Loop through the lowercase alphabet */
-	{
-		if (letter != 'e' && letter != 'q') /* Check if the letter is not 'e' or 'q' */
-		{
-			putchar(letter); /* Print the current letter using putchar */
-		}
-		letter++; /* Move to the next letter */
-	}
+    while (letter <= 'z') /* Loop through the lowercase alphabet */
+    {
+        if (letter != 'e' && letter != 'q') /* Check if the letter is not 'e' or 'q' */
+        {
+            putchar(letter); /* Print the current letter using putchar */
+        }
+        letter++; /* Move to the next letter */
+    }
 
-	putchar('\n'); /* Print a newline character to start a new line */
+    putchar('\n'); /* Print a newline character to start a new line */
 
-	return (0); /* Return 0 to indicate success */
+    return (0); /* Return 0 to indicate success */
 }
