393c5347f9bc8724599e99a4833326de9c3c4520
Doyin-dev <140111657+Doyin-dev@users.noreply.github.com>
2023-09-14 23:52:57 +0100

    Update 0-main.c Doyin-dev

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-/*
+/**
  * main - tests the positive_or_negative function
  * Return: Always 0
  */
@@ -8,8 +8,13 @@ int main(void)
 {
     int i;
 
-    i = 0;  /* Change this line to test with different numbers  */
+    i = 5;  // Should print "5 is positive"
+    positive_or_negative(i);
 
+    i = -2;  // Should print "-2 is negative"
+    positive_or_negative(i);
+
+    i = 0;  // Should print "0 is zero"
     positive_or_negative(i);
 
     return (0);
