ea5a8d2d8f5bf30a4b15d604a7aa4427ca14ed8c
Doyin-dev <140111657+Doyin-dev@users.noreply.github.com>
2023-09-15 00:04:10 +0100

    Update 0-main.c dev

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,16 @@
-#include "main.h"
-
-/*
- * main - tests the positive_or_negative function
- * Return: Always 0
- */
-int main(void)
-{
-    int i;
-
-    i = 0;  /* Change this line to test with different numbers */
-
-    positive_or_negative(i);
-
-    return (0);
-}
+#include "main.h" 
+  
+ /** 
+  * main - Test function for positive or negative 
+  * Return: 0 
+  */ 
+  
+ int main(void) 
+ { 
+ int i; 
+  
+ i = 0; 
+ positive_or_negative(i); 
+  
+ return (0); 
+ }
