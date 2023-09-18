2ee4cc7b89dbbf7b0ba58648e7acd309e83b3913
Doyin-dev <140111657+Doyin-dev@users.noreply.github.com>
2023-09-18 19:46:40 +0100

    Update 4-print_rev.c Doyin-dev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,24 +1,22 @@
-#include "main.h" 
- /** 
-  * print_rev - imprime en reversa 
-  * @s: string 
-  * return: 0 
-  */ 
- void print_rev(char *s) 
- { 
-         int longi = 0; 
-         int o; 
-  
-         while (*s != '\0') 
-         { 
-                 longi++; 
-                 s++; 
-         } 
-         s--; 
-         for (o = longi; o > 0; o--) 
-         { 
-                 _putchar(*s); 
-                 s--; 
-         } 
-         _putchar('\n'); 
- }
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse, followed by a new line
+ * @s: string to print in reverse
+ */
+void print_rev(char *s)
+{
+    int length = 0;
+
+    while (s[length] != '\0')
+    {
+        length++;
+    }
+
+    for (int i = length - 1; i >= 0; i--)
+    {
+        _putchar(s[i]);
+    }
+
+    _putchar('\n');
+}
