9a26a1048e7d63d96cb995c1f6a2d92935f84226
Doyin-dev <140111657+Doyin-dev@users.noreply.github.com>
2023-09-26 01:18:22 +0100

    Update 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,25 +1,18 @@
-/* 2-memcpy_rev.c */
-
-/**
- * _memcpy_rev - Copies n bytes from memory area src to memory area dest in reverse order.
- * @dest: A pointer to the destination memory area.
- * @src: A pointer to the source memory area.
- * @n: The number of bytes to copy.
- *
- * Return: A pointer to dest.
- */
-char *_memcpy_rev(char *dest, char *src, unsigned int n)
-{
-    char *start_dest = dest;
-    char *start_src = src;
-
-    dest += n - 1;
-    src += n - 1;
-
-    while (n--)
-    {
-        *dest-- = *src--;
-    }
-
-    return (start_dest);
-}
+#include "main.h" 
+ /** 
+  * _strchr - Entry point 
+  * @s: input 
+  * @c: input 
+  * Return: Always 0 (Success) 
+  */ 
+ char *_strchr(char *s, char c) 
+ { 
+         int i = 0; 
+  
+         for (; s[i] >= '\0'; i++) 
+         { 
+                 if (s[i] == c) 
+                         return (&s[i]); 
+         } 
+         return (0); 
+ }
