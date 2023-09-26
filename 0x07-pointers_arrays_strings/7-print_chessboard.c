826ab81e7c4db2552775204230e2689faa5bd2f2
Doyin-dev <140111657+Doyin-dev@users.noreply.github.com>
2023-09-26 02:09:12 +0100

    Update 7-print_chessboard.c

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,17 +1,22 @@
-#include "main.h" 
-  
- /** 
-  * print_chessboard - prints a chessboard 
-  * @chessboard: chessboard layout 
-  */ 
- void print_chessboard(char (*chessboard)[8]) 
- { 
-         int row, col; 
-  
-         for (row = 0; row < 8; row++) 
-         { 
-                 for (col = 0; col < 8; col++) 
-                         _putchar(chessboard[row][col]); 
-                 _putchar('\n'); 
-         } 
- }
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_chessboard - Prints a chessboard.
+ * @a: The 2D array representing the chessboard.
+ *
+ * Description: This function prints a chessboard represented by a 2D array.
+ */
+void print_chessboard(char (*a)[8])
+{
+    int row, col;
+
+    for (row = 0; row < 8; row++)
+    {
+        for (col = 0; col < 8; col++)
+        {
+            _putchar(a[row][col]); // Print each character
+        }
+        _putchar('\n'); // Print a newline after each row
+    }
+}
