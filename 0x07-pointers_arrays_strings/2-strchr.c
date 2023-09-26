24cfbb5b43bde04de673d2ace1b9deae178ee5b7
Doyin-dev <140111657+Doyin-dev@users.noreply.github.com>
2023-09-26 01:21:01 +0100

    Update 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,18 +1,22 @@
-#include "main.h" 
- /** 
-  * _strchr - Entry point 
-  * @s: input 
-  * @c: input 
-  * Return: Always 0 (Success) 
-  */ 
- char *_strchr(char *s, char c) 
- { 
-         int i = 0; 
-  
-         for (; s[i] >= '\0'; i++) 
-         { 
-                 if (s[i] == c) 
-                         return (&s[i]); 
-         } 
-         return (0); 
- }
+#include "main.h"
+
+/**
+ * _strchr - Locates a character in a string.
+ * @s: The string to search.
+ * @c: The character to locate.
+ *
+ * Return: A pointer to the first occurrence of the character c in the string s,
+ *         or NULL if the character is not found.
+ */
+char *_strchr(char *s, char c)
+{
+    int i = 0;
+
+    for (; s[i] != '\0'; i++)
+    {
+        if (s[i] == c)
+            return (&s[i]);
+    }
+
+    return (NULL);
+}
