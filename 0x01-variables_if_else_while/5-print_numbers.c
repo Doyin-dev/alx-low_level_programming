86b8e88a15c7c1bf076fbe0ac63bedcb743a7973
Doyin-dev <140111657+Doyin-dev@users.noreply.github.com>
2023-10-07 21:53:16 +0100

    Update 5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,5 +1,3 @@
-#include <stdio.h> 
-  
 /**
  * main - Entry point of the program
  *
@@ -9,11 +7,11 @@
  */
 int main(void)
 {
-	 int i;
+    int i;
 
-	 for (i = 0; i < 10; i++)
-		  printf("%d", i);
-	 printf("\n");
+    for (i = 0; i < 10; i++)
+        printf("%d", i);
+    printf("\n");
 
-	 return (0);
+    return (0);
 }
