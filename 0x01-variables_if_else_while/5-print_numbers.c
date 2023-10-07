047a381ff78d39e6acab7d2183f98957baa161b2
Doyin-dev <140111657+Doyin-dev@users.noreply.github.com>
2023-10-07 21:28:10 +0100

    Update 5-print_numbers.c Doyin-dev

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,17 +1,19 @@
 #include <stdio.h> 
   
- /** 
-  * main - printing first ten digits in base 10 
-  * 
-  * Return: 0 
-  */ 
- int main(void) 
- { 
-         int i; 
-  
-         for (i = 0; i < 10; i++) 
-                 printf("%d", i); 
-         printf("\n"); 
-  
-         return (0); 
- }
+/**
+ * main - Entry point of the program
+ *
+ * This function prints the first ten digits in base 10.
+ *
+ * Return: 0
+ */
+int main(void)
+{
+    int i;
+
+    for (i = 0; i < 10; i++)
+        printf("%d", i);
+    printf("\n");
+
+    return (0);
+}
