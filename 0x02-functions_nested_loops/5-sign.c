003a509493fb37935d701f3bce692999cc30df62
Doyin-dev <adedoyinvieve2021@gmail.com>
2023-10-10 12:29:27 -0700

    #include main.h
     /**
      * print_sign - Print the sign of a number
      * @n: number
      *
      * Return: 1 if positive, -1 if negative, 0 if zero
      */
     int print_sign(int n)
     {
             int sign = 0;
    
             if (n > 0)
             {
                     _putchar('+');
                     sign = 1;
             }
             if (n < 0)
             {
                     _putchar('-');
                     sign = -1;
             }
    
             /* n is 0 */
             _putchar('0');
    
             return (sign);

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h" 
+  
+ /** 
+  * print_sign - Print the sign of a number 
+  * @n: number 
+  * 
+  * Return: 1 if positive, -1 if negative, 0 if zero 
+  */ 
+ int print_sign(int n) 
+ { 
+         int sign = 0; 
+  
+         if (n > 0) 
+         { 
+                 _putchar('+'); 
+                 sign = 1; 
+         } 
+         if (n < 0) 
+         { 
+                 _putchar('-'); 
+                 sign = -1; 
+         } 
+  
+         /* n is 0 */ 
+         _putchar('0'); 
+  
+         return (sign); 
+ }
