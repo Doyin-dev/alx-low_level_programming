84072ec67813202914c85152fff456ffa2ba35dc
Doyin-dev <adedoyinvieve2021@gmail.com>
2023-10-10 08:06:27 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,16 @@
-#include <stdio.h>
-
-int main() {
-    printf("_putchar\n");
-    return 0;
-}
+#include "main.h" 
+  
+ /** 
+  * main - driver code to test _putchar() function 
+  * Return: 0 
+  */ 
+ int main(void) 
+ { 
+         char *str = "_putchar"; 
+  
+         for (; *str; str++) 
+                 _putchar(*str); 
+         _putchar('\n'); 
+  
+         return (0); 
+ }
