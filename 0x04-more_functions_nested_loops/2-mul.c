344cdb94eb95c91d01c80f371fba6775f91bd613
Doyin-dev <adedoyinvieve2021@gmail.com>
2023-10-12 09:42:59 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  * mul - returns the product of a and b
+  *
+  * @a: number 1
+  * @b: number 2
+  * Return: product of @a and @b
+  */
+ int mul(int a, int b)
+ {
+         return (a * b);
+ }
