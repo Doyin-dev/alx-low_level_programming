f4634933bc5e35ffeea1a9580cb4ab8d4fb28e77
Doyin-dev <140111657+Doyin-dev@users.noreply.github.com>
2023-10-23 13:41:37 +0100

    Update 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,20 +1,20 @@
-
-/** 
-  * _memset - fill memory with constant byte 
-  * @s: string buffer 
-  * @b: byte to write 
-  * @n: number of bytes to write 
+#include "main.h" 
+ /** 
+  * _memset - fill a block of memory with a specific value 
+  * @s: starting address of memory to be filled 
+  * @b: the desired value 
+  * @n: number of bytes to be changed 
   * 
-  * Description: The _memset() function fills the first @n bytes of the memory 
-  * area pointed to by @s with the constant byte @b. 
-  * 
-  * Return: pointer to string buffer @s 
+  * Return: changed array with new value for n bytes 
   */ 
  char *_memset(char *s, char b, unsigned int n) 
  { 
-         unsigned int i; 
+         int i = 0; 
   
-         for (i = 0; i < n; i++) 
-                 *(s + i) = b; 
+         for (; n > 0; i++) 
+         { 
+                 s[i] = b; 
+                 n--; 
+         } 
          return (s); 
  }
